e0ecb5595f65e85a5838aad51c2982bce0f704a3
金鹏 陶 <2530205209@qq.com>
2025-03-21 16:48:12 +0800

    Instance

--- LezRenderer-main/main.cpp
+++ LezRenderer-main/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -18,7 +19,7 @@ public:
     }
 
 private:
-    // Initialize the window
+    // 初始化窗口
     void initWindow() {
         glfwInit();
         // Do not create an OpenGL context
@@ -28,28 +29,74 @@ private:
         // the window(width, height, title, screenIndex, someting relevant to OpenGL)
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
-    // allocate the resources
+    // 分配资源
     void initVulkan() {
-
+        createInstance();
     }
 
     // 
     void mainLoop() {
         
         while (!glfwWindowShouldClose(window)) {
-            std::cout << "hello" << std::endl;
             glfwPollEvents();
         }
     }
 
-    // deallocate the resources
+    // 释放资源
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
     }
+
+
+    void createInstance() {
+        // 一个存储关于application相关信息的结构体
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO; // 明确该结构体所存储的信息类型
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        // 另一个结构体
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        // 判断VkInstance是否创建成功
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+        else {
+            std::cout << "Successful!" << std::endl;
+        }
+
+        uint32_t extensionCount = 0;
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());  
+
+        // 可用的扩展
+        std::cout << "available extensions:\n";
+
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+    }
 private:
     GLFWwindow* window; // the window to show
+    VkInstance instance; // vk实例
 };
 
 int main() {
